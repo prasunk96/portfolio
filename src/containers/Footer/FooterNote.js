3c4c0d3fd4d95ca53c1a6a884b8ce1d6f0dba809
prasunk <prasunk@avaya.com>
2021-05-27 20:02:51 +0530

    cleaned up code and made common css variables

--- src/containers/Footer/FooterNote.js
+++ src/containers/Footer/FooterNote.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 export const FooterNote = () => {
     return (
         <div className={styles.footerCopyRight}>
-            <FaRegCopyright color="#6d6d6d" size="10" className={styles.copyRrightIcon} />
+            <FaRegCopyright color="#929292" size="10" className={styles.copyRrightIcon} />
             Designed & build by Prasun Kumar
         </div>
     )
